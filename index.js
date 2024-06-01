a6cc96ee2898bb3b955d877fba4b5938956ea4e5
sangheon-k <sangheon.k@kakao.com>
2024-06-01 23:24:54 +0900

    feat : db 연결

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const db = require("./config/mysql");
+const conn = db.init();
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+app.get('/users', (req, res) => {
+  conn.query('SELECT * from Users', (error, rows) => {
+    if (error) throw error;
+    console.log('User info is: ', rows);
+    res.send(rows);
+  });
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
